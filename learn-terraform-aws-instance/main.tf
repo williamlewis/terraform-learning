68cbacfcfb1c742ac8dbf8ed1e369a181d914383
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-12-23 10:04:07 -0500

    Update main.tf with different AMI value

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,23 +1,24 @@
-terraform {#                                contains TF settings
-  required_providers {#                     providers used to provision infra
+terraform {            #                                contains TF settings
+  required_providers { #                     providers used to provision infra
     aws = {
-      source  = "hashicorp/aws"#            optional hostname, namespace, & provider type; installed from TF Registry by default
-      version = "~> 4.16"#                  version contrainst; optional but recommended to ensure compatibility with configuration (if none, TF will use latest provider version by default)
+      source  = "hashicorp/aws" #            optional hostname, namespace, & provider type; installed from TF Registry by default
+      version = "~> 4.16"       #                  version contrainst; optional but recommended to ensure compatibility with configuration (if none, TF will use latest provider version by default)
     }
   }
 
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {#                           configures the "provider" plugin; multiple providers can be specified
-  region  = "us-west-2"
+provider "aws" { #                           configures the "provider" plugin; multiple providers can be specified
+  region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {#     "Resource Type" "Resource Name" (Id will be "aws_instance.app_server"); define components of infra (virtual or physical)
-  ami           = "ami-830c94e3"#           image Id for Ubuntu OS
-  instance_type = "t2.micro"#               instance type (AWS free tier)
+resource "aws_instance" "app_server" { #        "Resource Type" "Resource Name" (Id will be "aws_instance.app_server"); define components of infra (virtual or physical)
+  # ami           = "ami-830c94e3"     #        ORIGINAL image Id for Ubuntu OS
+  ami           = "ami-08d70e59c07c61a3a"       #        NEW AMI to test applying a resource update
+  instance_type = "t2.micro"           #               instance type (AWS free tier)
 
   tags = {
-    Name = "ExampleAppServerInstance"#      name for instance
+    Name = "ExampleAppServerInstance" #         name for instance
   }
 }
