ac24e45ffe412bcca8b7d92ce8594b74a291276e
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-12-23 09:13:05 -0500

    Include comments for main.tf config aspects

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,23 +1,23 @@
-terraform {
-  required_providers {
+terraform {#                                contains TF settings
+  required_providers {#                     providers used to provision infra
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+      source  = "hashicorp/aws"#            optional hostname, namespace, & provider type; installed from TF Registry by default
+      version = "~> 4.16"#                  version contrainst; optional but recommended to ensure compatibility with configuration (if none, TF will use latest provider version by default)
     }
   }
 
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
+provider "aws" {#                           configures the "provider" plugin; multiple providers can be specified
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_instance" "app_server" {#     "Resource Type" "Resource Name" (Id will be "aws_instance.app_server"); define components of infra (virtual or physical)
+  ami           = "ami-830c94e3"#           image Id for Ubuntu OS
+  instance_type = "t2.micro"#               instance type (AWS free tier)
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ExampleAppServerInstance"#      name for instance
   }
 }
